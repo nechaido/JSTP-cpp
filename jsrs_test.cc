612b9348a6c2a860672df68e284393a97be16807
Dmytro Nechai <nechaido@gmail.com>
2016-03-28 19:39:32 +0300

    Test fix

--- jsrs_test.cc
+++ jsrs_test.cc
@@ -1,6 +1,15 @@
 #include "gtest/gtest.h"
 #include "jsrs.h"
 
+int checksum(std::string s){//TODO implement undefined
+  int result = 0;
+  for (auto &sIt : s){
+    if (sIt != ' ' && sIt != '\n')
+      result ^= sIt;
+  }
+  return result;
+}
+
 TEST(jsrs_test, jsrs_test_dump_Test1) {
   jstp::JSRS t;
   EXPECT_EQ(t.dump(), "undefined");
@@ -62,3 +71,103 @@ TEST(jsrs_test, jsrs_test_dump_Test8) {
   EXPECT_EQ(t.dump(), "{arr:[25.5,true],test1:25.5,test2:true,test3:\"test\"}");
 }
 
+TEST(jsrs_test, jsrs_test_dump_TestShouldWork) {
+  std::vector<std::string> arr;
+  arr.push_back("{\n"
+                    "  name:\"Marcus Aurelius\",\n"
+                    "  passport:\"AE127095\",\n"
+                    "  birth:{\n"
+                    "    date:\"1990-02-15\",\n"
+                    "    place:\"Rome\"\n"
+                    "  },\n"
+                    "  contacts:{\n"
+                    "    email:\"marcus@aurelius.it\",\n"
+                    "    phone:\"+380505551234\",\n"
+                    "    address:{\n"
+                    "      country:\"Ukraine\",\n"
+                    "      city:\"Kiev\",\n"
+                    "      zip:\"03056\",\n"
+                    "      street:\"Pobedy\",\n"
+                    "      building:\"37\",\n"
+                    "      floor:\"1\",\n"
+                    "      room:\"158\"\n"
+                    "    }\n"
+                    "  }\n"
+                    "}");
+  arr.push_back("{\n"
+                    "  obj:{\n"
+                    "    obj:{\n"
+                    "      obj:{\n"
+                    "        obj:{\n"
+                    "          obj:{\n"
+                    "            obj:{\n"
+                    "              obj:{\n"
+                    "                obj:{\n"
+                    "                  obj:{\n"
+                    "                    obj:{\n"
+                    "                      obj:{\n"
+                    "                        obj:{\n"
+                    "                          obj:{\n"
+                    "                            obj:{\n"
+                    "                              name: \"name\"\n"
+                    "                            },\n"
+                    "                              name: \"name\"\n"
+                    "                          },\n"
+                    "                            name: \"name\"\n"
+                    "                        },\n"
+                    "                          name: \"name\"\n"
+                    "                      },\n"
+                    "                        name: \"name\"\n"
+                    "                    },\n"
+                    "                      name: \"name\"\n"
+                    "                  },\n"
+                    "                    name: \"name\"\n"
+                    "                },\n"
+                    "                  name: \"name\"\n"
+                    "              },\n"
+                    "              name: \"name\"\n"
+                    "            },\n"
+                    "            name: \"name\"\n"
+                    "          },\n"
+                    "          name: \"name\"\n"
+                    "        },\n"
+                    "        name: \"name\"\n"
+                    "      },\n"
+                    "      name: \"name\"\n"
+                    "    },\n"
+                    "    name: \"name\"\n"
+                    "  },\n"
+                    "  name : null\n"
+                    "}");
+  arr.push_back("{\n"
+                    "  me: {\n"
+                    "    you1 : [[\"Yep\", \"Yep\", \"Yup\"],, {here_it_is : 42},, 3, \"it`s\"],\n"
+                    "    you2 : [[\"Yep\", \"Yep\", \"Yup\"],, {here_it_is : 42},, 3, \"it`s\"]\n"
+                    "  },\n"
+                    "  you : [[\"Yep\", \"Yep\", \"Yup\"],, {here_it_is : 42},, 3, \"it`s\"]\n"
+                    "}"); //TODO Make it work, Timur said so
+  arr.push_back("{\n"
+                    "  me : [,\"me\",]\n"
+                    "}"); //TODO Make it work, Timur said so
+  arr.push_back("{}"); //TODO Make it work, Timur said so
+  arr.push_back("[]"); //TODO Make it work, Timur said so
+  for (auto &iterator : arr) {
+    std::string err = "";
+    jstp::JSRS jsrs = jstp::JSRS::parse(iterator, err);
+    EXPECT_EQ("", err);
+    if(checksum(jsrs.dump()) == checksum(iterator)){
+      EXPECT_EQ(iterator, jsrs.dump());
+    }
+  }
+}
+
+TEST(jsrs_test, jsrs_test_dump_TestShouldNotWork) {
+  std::vector<std::string> arr;
+  arr.push_back("{n: nult}"); //TODO This should throw an error, not kill a programm
+  arr.push_back("{n:}");
+  for (auto &iterator : arr) {
+    std::string err = "";
+    jstp::JSRS jsrs = jstp::JSRS::parse(iterator, err);
+    EXPECT_NE("", err);
+  }
+}
